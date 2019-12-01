3f61f4a63522fd3867cf084540cd9e35c035e12f
Michael Mescheder <meschedermichael@gmail.com>
2019-11-30 23:30:30 -0500

    Created Project & Day 1 Class
    Created a new AdventOfCode project, creating a program.cs file to run each day's solution. Created the Day1.cs file in preparation for the first question.

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)     // Functions to test each solution will be called here
+        {
+            Day1.outputSolution();    // Solution to the day 1 question. Output should match the example provided with the question. Data input is handled in the respective class
+        }
+    }
+}

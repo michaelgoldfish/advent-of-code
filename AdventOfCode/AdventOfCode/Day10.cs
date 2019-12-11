3f243d0593c3b3a3f5c6748a6917d846b0fd1df4
Michael Mescheder <meschedermichael@gmail.com>
2019-12-11 04:09:39 -0500

    Day 10 Moved Part 1 to Function
    Moved the OutputSolution code to a separate function, and added an empty function with comments that will be used to complete part 2.

--- AdventOfCode/AdventOfCode/Day10.cs
+++ AdventOfCode/AdventOfCode/Day10.cs
@@ -122,7 +122,80 @@ namespace AdventOfCode
             ###.##.####.##.#..##
 
             Find the best location for a new monitoring station. How many other asteroids can be detected from that location?
-            Answer: 214
+            Answer: 214 (at position [8,16]
+        */
+
+        /*  --- Part Two ---
+        
+            Once you give them the coordinates, the Elves quickly deploy an Instant Monitoring Station to the location and discover the worst: there are simply too many asteroids.
+
+            The only solution is complete vaporization by giant laser.
+
+            Fortunately, in addition to an asteroid scanner, the new monitoring station also comes equipped with a giant rotating laser perfect for vaporizing asteroids.
+            The laser starts by pointing up and always rotates clockwise, vaporizing any asteroid it hits.
+
+            If multiple asteroids are exactly in line with the station, the laser only has enough power to vaporize one of them before continuing its rotation.
+            In other words, the same asteroids that can be detected can be vaporized, but if vaporizing one asteroid makes another one detectable,
+            the newly-detected asteroid won't be vaporized until the laser has returned to the same position by rotating a full 360 degrees.
+
+            For example, consider the following map, where the asteroid with the new monitoring station (and laser) is marked X:
+
+            .#....#####...#..
+            ##...##.#####..##
+            ##...#...#.#####.
+            ..#.....X...###..
+            ..#.#.....#....##
+
+            The first nine asteroids to get vaporized, in order, would be:
+
+            .#....###24...#..
+            ##...##.13#67..9#
+            ##...#...5.8####.
+            ..#.....X...###..
+            ..#.#.....#....##
+
+            Note that some asteroids (the ones behind the asteroids marked 1, 5, and 7) won't have a chance to be vaporized until the next full rotation.
+            The laser continues rotating; the next nine to be vaporized are:
+
+            .#....###.....#..
+            ##...##...#.....#
+            ##...#......1234.
+            ..#.....X...5##..
+            ..#.9.....8....76
+
+            The next nine to be vaporized are then:
+
+            .8....###.....#..
+            56...9#...#.....#
+            34...7...........
+            ..2.....X....##..
+            ..1..............
+
+            Finally, the laser completes its first full rotation (1 through 3), a second rotation (4 through 8),
+            and vaporizes the last asteroid (9) partway through its third rotation:
+
+            ......234.....6..
+            ......1...5.....7
+            .................
+            ........X....89..
+            .................
+
+            In the large example above (the one with the best monitoring station location at 11,13):
+
+            -The 1st asteroid to be vaporized is at 11,12.
+            -The 2nd asteroid to be vaporized is at 12,1.
+            -The 3rd asteroid to be vaporized is at 12,2.
+            -The 10th asteroid to be vaporized is at 12,8.
+            -The 20th asteroid to be vaporized is at 16,0.
+            -The 50th asteroid to be vaporized is at 16,9.
+            -The 100th asteroid to be vaporized is at 10,16.
+            -The 199th asteroid to be vaporized is at 9,6.
+            -The 200th asteroid to be vaporized is at 8,2.
+            -The 201st asteroid to be vaporized is at 10,9.
+            -The 299th and final asteroid to be vaporized is at 11,1.
+
+            The Elves are placing bets on which will be the 200th asteroid to be vaporized. Win the bet by determining which asteroid that will be; what do you get if you multiply its X coordinate by 100 and then add its Y coordinate? (For example, 8,2 becomes 802.)
+            Answer: 
         */
 
         private static int[,] InstantiateMap()  
@@ -299,12 +372,11 @@ namespace AdventOfCode
             return n;
         }
 
-        public static void OutputSolution()
+        private static void HighestVisibilityAsteroid(int[,] map)
         {
-            int[,] map = InstantiateMap();
-
             // Find the highest number of asteroids that can be seen from a single asteroid
             int mostAsteroidsSeen = 0;
+            int[] mostAsteroidsPosition = { 0, 0 };
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for (int j = 0; j < map.GetLength(1); j++)
@@ -315,13 +387,38 @@ namespace AdventOfCode
                         if (asteroids > mostAsteroidsSeen)
                         {
                             mostAsteroidsSeen = asteroids;
+                            int[] position = { j, i };
+                            mostAsteroidsPosition = position;
                         }
                     }
-
                 }
             }
 
-            Console.WriteLine("The highest number of asteroids seen is: {0}", mostAsteroidsSeen);
+            Console.WriteLine("The highest number of asteroids seen is {0} at position [{1},{2}]", mostAsteroidsSeen, mostAsteroidsPosition[0], mostAsteroidsPosition[1]);
+        }
+
+        private static int[] VaporizedAt(int[,] map, int[] laser, int n)   // Returns the location of the nth asteroid to get vaporized
+        {
+            int[] asteroidPosition = new int[2];
+
+            //TODO vaporize asteroids clockwise
+
+            // Possible idea: compare the asteroid's positions as fractions in order to figure out which ones get reached first.
+            // Then on a tie do the same as part 1 to figure out which asteroid gets vaporized first. Maybe save the non-vaporized in a separate list to be looped through once the laser is pointing straight up again
+
+            return asteroidPosition;
+        }
+
+        public static void OutputSolution()     // Laser position = [8,16]
+        {
+            int[,] map = InstantiateMap();
+            int[] laser = { 8, 16 };
+            const int asteroidBet = 200;
+
+            int[] asteroidPos = VaporizedAt(map, laser, asteroidBet);
+            int math = asteroidPos[0] * 100 + asteroidPos[1];
+
+            Console.WriteLine("Arbitrary math solution: {0}", math);
         }
     }
 }
